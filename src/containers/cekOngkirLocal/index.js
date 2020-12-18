ecaab318672a0c1f98137aa3122a88465713b5ef
thisWandiPratama <wandi99pratama@gmail.com>
2020-12-18 17:27:59 +0700

    Menambahkan list kota dan kecamatan dari kota penerima

--- src/containers/cekOngkirLocal/index.js
+++ src/containers/cekOngkirLocal/index.js
@@ -16,12 +16,20 @@ import Header from '../../components/hedear';
 const Tracking = ({navigation}) => {
   const [Kota, setKota] = useState ('');
   const [kotaID, setkotaID] = useState ();
+  const [KotaTujuan, setKotaTujuan] = useState ('');
+  const [kotaIDTujuan, setkotaIDTujuan] = useState ();
+  const [jenisID, setJenisID] = useState ();
   const [kecamatan, setKecamatan] = useState ([]);
-  const [kecamatanID, setKecamatanID] = useState ([]);
+  const [kecamatanID, setKecamatanID] = useState ();
+  const [kecamatanTujuan, setKecamatanTujuan] = useState ('');
+  const [kecamatanIDTujuan, setKecamatanIDTujuan] = useState ();
   const [allKota, setAllKota] = useState ([]);
   const [allKecamatan, setAllKecamatan] = useState ([]);
+  const [allKecamatanTujuan, setAllKecamatanTujuan] = useState ([]);
   const [modalKota, setModalKota] = useState (false);
   const [modalKecamatan, setModalKecamatan] = useState (false);
+  const [modalKotaTujuan, setModalKotaTujuan] = useState (false);
+  const [modalKecamatanTujuan, setModalKecamatanTujuan] = useState (false);
 
   useEffect (() => {
     getKota ();
@@ -29,7 +37,7 @@ const Tracking = ({navigation}) => {
       if (allKota.length > 0) {
         getKecamatan ();
       }
-    }, 2000);
+    }, 3000);
   }, []);
 
   const onPress = () => {
@@ -66,6 +74,23 @@ const Tracking = ({navigation}) => {
     });
   };
 
+  const renderListKotaTujuan = () => {
+    return allKota.map ((value, index) => {
+      return (
+        <TouchableOpacity
+          key={index}
+          onPress={() => {
+            setKotaTujuan (value.city_name);
+            setkotaIDTujuan (value.city_id);
+            setModalKotaTujuan (false);
+          }}
+        >
+          <Text style={styles.namaKota}>{value.city_name}</Text>
+        </TouchableOpacity>
+      );
+    });
+  };
+
   const getKecamatan = () => {
     fetch (`https://api.rajaongkir.sipondok.com/v1/kecamatan/kota/${kotaID}`)
       .then (result => result.json ())
@@ -73,6 +98,18 @@ const Tracking = ({navigation}) => {
         setAllKecamatan (result);
         setKecamatan (result[0].subdistrict_name);
         setKecamatanID (result[0].subdistrict_id);
+        console.log (result);
+      })
+      .catch (error => console.log (error));
+  };
+
+  const getKecamatanTujuan = () => {
+    fetch (`https://api.rajaongkir.sipondok.com/v1/kecamatan/kota/${kotaIDTujuan}`)
+      .then (result => result.json ())
+      .then (result => {
+        setAllKecamatanTujuan (result);
+        setKecamatanTujuan (result[0].subdistrict_name);
+        setKecamatanIDTujuan (result[0].subdistrict_id);
         console.log (result);
       })
       .catch (error => console.log (error));
@@ -87,6 +124,24 @@ const Tracking = ({navigation}) => {
             setKecamatan (value.subdistrict_name);
             setKecamatanID (value.subdistrict_id);
             setModalKecamatan (false);
+          }}
+        >
+          <Text style={styles.namaKota}>{value.subdistrict_name}</Text>
+        </TouchableOpacity>
+      );
+    });
+  };
+
+  const renderListKecamatanTujuan = () => {
+    return allKecamatanTujuan.map ((value, index) => {
+      return (
+        <TouchableOpacity
+          key={index}
+          onPress={() => {
+            setKecamatanTujuan (value.subdistrict_name);
+            setKecamatanIDTujuan (value.subdistrict_id);
+            setJenisID ('kecamatan');
+            setModalKecamatanTujuan (false);
           }}
         >
           <Text style={styles.namaKota}>{value.subdistrict_name}</Text>
@@ -141,6 +196,57 @@ const Tracking = ({navigation}) => {
           </View>
         </View>
       </Modal>
+
+      <Modal animationType="slide" transparent={true} visible={modalKotaTujuan}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>
+              Pilih Kota Tujuan Anda
+            </Text>
+            <TouchableHighlight
+              style={styles.cancel}
+              onPress={() => {
+                setModalKotaTujuan (!modalKotaTujuan);
+              }}
+            >
+              <Text style={styles.titleCancel}>X</Text>
+            </TouchableHighlight>
+            <ScrollView>
+              <View style={styles.listKota}>
+                {renderListKotaTujuan ()}
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalKecamatanTujuan}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>
+              Pilih Kecamatan Tujuan Anda
+            </Text>
+            <TouchableHighlight
+              style={styles.cancel}
+              onPress={() => {
+                setModalKecamatanTujuan (!modalKecamatanTujuan);
+              }}
+            >
+              <Text style={styles.titleCancel}>X</Text>
+            </TouchableHighlight>
+            <ScrollView>
+              <View style={styles.listKota}>
+                {renderListKecamatanTujuan ()}
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      </Modal>
+
       <Header onPress={onPress} title="Rates Local" />
       <View style={styles.boxContainer}>
         <View style={styles.boxInput}>
@@ -150,7 +256,12 @@ const Tracking = ({navigation}) => {
             </View>
             <View style={{flexDirection: 'row'}}>
               <TouchableOpacity
-                onPress={() => setModalKota (true)}
+                onPress={() => {
+                  if (allKota.length === 0) {
+                    getKota ();
+                  }
+                  setModalKota (true);
+                }}
                 style={styles.boxItem}
               >
                 <Text style={styles.listOrigin}>
@@ -160,7 +271,12 @@ const Tracking = ({navigation}) => {
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => setModalKecamatan (true)}
+                onPress={() => {
+                  if (allKecamatan.length === 0) {
+                    getKecamatan ();
+                  }
+                  setModalKecamatan (true);
+                }}
                 style={styles.boxItem}
               >
                 <Text style={styles.listOrigin}>
@@ -172,8 +288,36 @@ const Tracking = ({navigation}) => {
             </View>
           </View>
           <View style={styles.boxList}>
-            <Text style={styles.title}>Destination</Text>
-            <TextInput placeholder="Masukan No.Resi" style={styles.input} />
+            <View style={styles.boxOrigin}>
+              <Text style={styles.title}>Destination</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                onPress={() => setModalKotaTujuan (true)}
+                style={styles.boxItem}
+              >
+                <Text style={styles.listOrigin}>
+                  {' '}
+                  {KotaTujuan.length === 0 ? 'Pilih Kota' : KotaTujuan}
+                  {' '}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  getKecamatanTujuan()
+                  setModalKecamatanTujuan (true);
+                }}
+                style={styles.boxItem}
+              >
+                <Text style={styles.listOrigin}>
+                  {' '}
+                  {kecamatanTujuan.length === 0
+                    ? 'Pilih Kecamatan'
+                    : kecamatanTujuan}
+                  {' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
           <View style={styles.boxList}>
             <Text style={styles.title}>Berat(g)</Text>
